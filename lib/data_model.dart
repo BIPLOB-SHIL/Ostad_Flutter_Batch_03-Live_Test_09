7e51d1dc893f67e9b2086c476450c9d8860af2cc
BIPLOB-SHIL <biplobshil.duet@gmail.com>
2023-07-11 01:58:56 +0600

    Try a second time after the internet is back on

--- lib/data_model.dart
+++ lib/data_model.dart
@@ -1,56 +1,24 @@
 class DataModel {
-  DataModel({
-      this.recipes,});
+  late String title;
+  late  String description;
+  late  List<String> ingredients;
 
-  DataModel.fromJson(dynamic json) {
-    if (json['recipes'] != null) {
-      recipes = [];
-      json['recipes'].forEach((v) {
-        recipes?.add(Recipes.fromJson(v));
-      });
-    }
-  }
-  List<Recipes>? recipes;
-DataModel copyWith({  List<Recipes>? recipes,
-}) => DataModel(  recipes: recipes ?? this.recipes,
-);
-  Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{};
-    if (recipes != null) {
-      map['recipes'] = recipes?.map((v) => v.toJson()).toList();
-    }
-    return map;
-  }
+  DataModel(this.title, this.description, this.ingredients);
 
-}
-
-class Recipes {
-  Recipes({
-      this.title, 
-      this.description, 
-      this.ingredients,});
-
-  Recipes.fromJson(dynamic json) {
+  DataModel.fromJson(Map<String,dynamic> json)
+  {
     title = json['title'];
-    description = json['description'];
-    ingredients = json['ingredients'] != null ? json['ingredients'].cast<String>() : [];
+    description =json['description'];
+    ingredients = json['ingredients'];
   }
-  String? title;
-  String? description;
-  List<String>? ingredients;
-Recipes copyWith({  String? title,
-  String? description,
-  List<String>? ingredients,
-}) => Recipes(  title: title ?? this.title,
-  description: description ?? this.description,
-  ingredients: ingredients ?? this.ingredients,
-);
-  Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{};
-    map['title'] = title;
-    map['description'] = description;
-    map['ingredients'] = ingredients;
-    return map;
-  }
+
+  Map<String,dynamic> toMap(){
+    return {
+      "title" : title,
+      "description": description,
+      "ingredients": ingredients,
+
+    };
+}
 
 }
